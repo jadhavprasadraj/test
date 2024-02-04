06f4537d95258fed647ca923bb66fca1536f1030
Prasad <jadhavprasadraj@gmail.com>
2024-02-04 19:28:57 +0530

    live update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New changes made KRS
+          New changes made for live demo
         </p>
         <a
           className="App-link"
